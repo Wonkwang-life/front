ce02a7ac60dfa9c3f83ce1090b892bfb8b826c1a
nemokoala <fdxguy2@gmail.com>
2024-06-13 19:07:43 +0900

    fix: 홈화면 제품목록 수정

--- src/components/HomeProduct.tsx
+++ src/components/HomeProduct.tsx
@@ -24,7 +24,7 @@ const HomeProduct = ({
       <ProductImg src={imgUrls} />
       <ProductTitle>{title}</ProductTitle>
       <ProductContent>{content}</ProductContent>
-      <ProductTag>{tag}</ProductTag>
+      {/* <ProductTag>{tag}</ProductTag> */}
     </Container>
   );
 };
@@ -36,42 +36,40 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   width: 30%;
-  gap: 10px;
+  gap: 15px;
 
   &:hover {
     cursor: pointer;
   }
 
-  @media screen and (max-width: 600px) {
+  @media screen and (max-width: 800px) {
     width: 100%;
+    padding-bottom: 30px;
+    border-bottom: 1px #868686 solid;
   }
 `;
 
 const ProductTitle = styled.span`
-  font-size: 1.3rem;
+  font-size: 1.4rem;
+  font-weight: 600;
 `;
 
 const ProductImg = styled.img`
   width: 100%;
-  max-width: 300px;
-  height: 100%;
+  height: 280px;
   object-fit: cover;
+
+  @media screen and (max-width: 800px) {
+    height: 400px;
+  }
 `;
 
 const ProductContent = styled.p`
   font-size: 1.1rem;
   width: 90%;
-
-  @media screen and (max-width: 800px) {
-    font-size: 0.8rem;
-  }
 `;
 const ProductTag = styled.p`
   font-size: 0.9rem;
   width: 90%;
   word-spacing: 15px;
-
-  @media screen and (max-width: 800px) {
-    font-size: 0.8rem;
-  }
 `;
